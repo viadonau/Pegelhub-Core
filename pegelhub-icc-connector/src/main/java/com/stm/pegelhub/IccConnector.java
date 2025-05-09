7e4118c37f85f3fdd95d8828351d2ece5f94fd53
Armin Grebić <armin.grebic@outlook.com>
2025-05-09 19:19:36 +0200

    add: create pipeline and add dockerfiles

--- pegelhub-icc-connector/src/main/java/com/stm/pegelhub/IccConnector.java
+++ pegelhub-icc-connector/src/main/java/com/stm/pegelhub/IccConnector.java
@@ -4,7 +4,6 @@ import com.stm.pegelhub.lib.PegelHubCommunicator;
 import com.stm.pegelhub.lib.PegelHubCommunicatorFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
