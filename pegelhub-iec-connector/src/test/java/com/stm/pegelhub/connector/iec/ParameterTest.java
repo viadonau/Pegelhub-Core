65f764b8e4c1f18e833801812e7c570b6ab47210
SgcSwitch666 <2310859004@fh-burgenland.at>
2025-05-11 16:26:14 +0200

    add unit tests

--- pegelhub-iec-connector/src/test/java/com/stm/pegelhub/connector/iec/ParameterTest.java
+++ pegelhub-iec-connector/src/test/java/com/stm/pegelhub/connector/iec/ParameterTest.java
@@ -3,14 +3,49 @@ package com.stm.pegelhub.connector.iec;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.time.Duration;
-import java.util.UUID;
-
-import static com.stm.pegelhub.connector.iec.Main.readArguments;
 
 public class ParameterTest {
 
-}
+    @Test
+    void testReadArguments_validInput() throws Exception {
+        String[] args = {"src/main/resources/config.properties"};
 
+        ConnectorOptions params = Main.readArguments(args);
+
+        assertNotNull(params);
+        assertEquals("properties.yaml", params.propertyFileName());
+        assertTrue(params.isReadingFromIec());
+        assertEquals(InetAddress.getByName("192.168.2.29"), params.coreAddress());
+        assertEquals(8081, params.corePort());
+        assertEquals(2404, params.iec_port());
+        assertEquals(1, params.common_address());
+        assertEquals(1, params.start_dt_retries());
+        assertEquals(20000, params.connection_timeout());
+        assertEquals(5000, params.message_fragment_timeout());
+        assertEquals(Duration.parse("PT5000S"), params.telemetryCycleTime());
+        assertEquals("PT30S", params.delay().toString());
+        assertEquals("nr1", params.stationNumbers().get(0));
+    }
+
+    @Test
+    void testReadArguments_missingArgument() throws IOException {
+        String path = "src/test/resources/test.properties";
+        String[] args = { path };
+        ConnectorOptions params = Main.readArguments(args);
+
+        assertEquals("localhost/127.0.0.1", params.iec_host().toString());
+    }
+
+
+    @Test
+    void testReadArguments_invalidFormat() throws IOException {
+        String[] args = {"notAParameter"};
+
+        assertThrows(IOException.class, () -> {
+            Main.readArguments(args);
+        });
+    }
+}
