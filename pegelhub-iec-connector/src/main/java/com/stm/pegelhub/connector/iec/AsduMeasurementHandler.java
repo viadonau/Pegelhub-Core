e4f3c4054e079df3ac078eaecc040607eabd990a
SgcSwitch666 <2310859004@fh-burgenland.at>
2025-05-25 10:24:47 +0200

    rework class IecClientListener

---
+++ pegelhub-iec-connector/src/main/java/com/stm/pegelhub/connector/iec/AsduMeasurementHandler.java
@@ -0,0 +1,107 @@
+package com.stm.pegelhub.connector.iec;
+
+import com.stm.pegelhub.lib.InfluxID;
+import com.stm.pegelhub.lib.PegelHubCommunicator;
+import com.stm.pegelhub.lib.internal.ApplicationPropertiesImpl;
+import com.stm.pegelhub.lib.model.Measurement;
+import com.stm.pegelhub.lib.model.Supplier;
+import org.openmuc.j60870.ASdu;
+import org.openmuc.j60870.ie.IeScaledValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.OffsetDateTime;
+import java.util.*;
+
+/**
+ * Handler for incoming ASdu messages (specifically M_ME_NB_1) and converts them into measurements
+ * to be sent to Pegelhub Core.
+ */
+public class AsduMeasurementHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AsduMeasurementHandler.class);
+
+    private final PegelHubCommunicator communicator;
+    private final ApplicationPropertiesImpl properties;
+    private final InfluxID influxID;
+
+    public AsduMeasurementHandler(PegelHubCommunicator communicator,
+                                  ConnectorOptions options,
+                                  ApplicationPropertiesImpl properties) {
+        this.communicator = communicator;
+        this.properties = properties;
+        this.influxID = new InfluxID(communicator, properties);
+    }
+
+    /**
+     * Processes the received ASdu based on its type.
+     * @param aSdu the received ASdu
+     */
+    public void handle(ASdu aSdu) {
+        switch (aSdu.getTypeIdentification()) {
+            case M_ME_NB_1 -> handleMeasurement(aSdu);
+            default -> logUnhandledType(aSdu);
+        }
+    }
+
+    /**
+     * Extracts measurement data from ASdu of type M_ME_NB_1 and sends them to Pegelhub.
+     * @param aSdu the received ASdu
+     */
+    private void handleMeasurement(ASdu aSdu) {
+        influxID.calculateID();
+        List<Measurement> measurements = new ArrayList<>();
+        Supplier supplier = findSupplier();
+        if (supplier == null) return;
+
+        for (var infoObj : aSdu.getInformationObjects()) {
+            int ioa = infoObj.getInformationObjectAddress();
+            var elements = infoObj.getInformationElements();
+
+            double value = ((IeScaledValue) elements[0][0]).getNormalizedValue();
+            int raw = ((IeScaledValue) elements[0][0]).getUnnormalizedValue();
+            String quality = elements[0][1].toString();
+            OffsetDateTime now = OffsetDateTime.now();
+
+            Map<String, Double> fields = Map.of("NormalizedValue", value);
+            Map<String, String> infos = Map.ofEntries(
+                    Map.entry("ChannelUse", supplier.getChannelUse()),
+                    Map.entry("Type", supplier.getChannelUse()),
+                    Map.entry("Timetype", "TODO"),
+                    Map.entry("ID", String.valueOf(influxID.getIDValue())),
+                    Map.entry("IOA", String.valueOf(ioa)),
+                    Map.entry("UnNormalizedValue", String.valueOf(raw)),
+                    Map.entry("Quality", quality),
+                    Map.entry("TimestampWithOffset", now.toString())
+            );
+
+            Measurement measurement = new Measurement(fields, infos);
+            measurements.add(measurement);
+            influxID.addID();
+
+            LOG.info("Received IEC measurement: IOA={}, Value={}, Quality={}", ioa, value, quality);
+        }
+
+        communicator.sendMeasurements(measurements);
+    }
+
+    /**
+     * Finds the supplier for the current connector.
+     * @return the matched Supplier or null if not found
+     */
+    private Supplier findSupplier() {
+        return communicator.getSuppliers().stream()
+                .filter(s -> s.getStationNumber().equals(properties.getSupplier().stationNumber()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Logs ASdus that are not handled by the system.
+     * @param aSdu the unhandled ASdu
+     */
+    private void logUnhandledType(ASdu aSdu) {
+        LOG.info("Unhandled ASdu Type: {}, Description: {}", aSdu.getTypeIdentification(), aSdu.getTypeIdentification().getDescription());
+    }
+}
+
